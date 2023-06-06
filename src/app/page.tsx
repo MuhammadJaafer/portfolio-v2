8ba5ced49d272ac31a3b131dd68c8f4a77dbe2ca
Muhammad Jaafar <mohamadjafer14235@gmail.com>
2023-06-06 21:03:06 +0300

    add firebase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
+"use client";
 import About from "@/components/About/About";
 import Contact from "@/components/Contact/Contact";
 import Hero from "@/components/Hero/Hero";
 import Projects from "@/components/Projects/Projects";
 import Skills from "@/components/Skills/Skills";
+import { analytics } from "@/firebase/config";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    analytics;
+  }, []);
   return (
     <>
       <Hero />
